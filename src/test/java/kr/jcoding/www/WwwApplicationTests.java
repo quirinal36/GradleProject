555c18f0d4c01a009d9c641894bd6632a799a380
leehg <turboguy36@gmail.com>
2021-01-24 00:27:00 +0900

    Initial commit

---
+++ src/test/java/kr/jcoding/www/WwwApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.jcoding.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WwwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
